cdb3403ce03ddf1bbd190fb9abe9ad5c6b5adb9e
Daniel Wang <danielwzhg@gmail.com>
2024-01-25 13:07:09 +0000

    check index cache

--- megatron/data/gpt_dataset.py
+++ megatron/data/gpt_dataset.py
@@ -145,6 +145,7 @@ def _build_train_valid_test_datasets(data_prefix, data_impl, splits_string,
         if splits[index + 1] > splits[index]:
             documents = np.arange(start=splits[index], stop=splits[index + 1],
                                   step=1, dtype=np.int32)
+            time.sleep(10)
             dataset = GPTDataset(name, data_prefix, documents, indexed_dataset,
                                  splits_string,
                                  train_valid_test_num_samples[index],
